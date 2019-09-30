26de6df55500cf45229192a05bdd32f97c4405e6
nnao45 <n4sekai5y@gmail.com>
2019-09-30 09:10:00 +0900

    kotlinでへろーワールド

--- src/Application.kt
+++ src/Application.kt
@@ -1,13 +1,21 @@
-package api.qicoo
+package com.example
 
 import io.ktor.application.*
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
-@Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respond(HttpStatusCode.OK,"Hello, Kotlin")
+            }
+        }
+    }
+    server.start()
 }
-
