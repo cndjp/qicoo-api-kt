41c6cc84fa25e9a922e7a6a5ae83f9cc63bc378a
nnao45 <n4sekai5y@gmail.com>
2019-10-03 16:35:31 +0900

    　.use使ってみる

--- src/main/kotlin/cndjp/qicoo/infrastructure/cache/context/RedisContext.kt
+++ src/main/kotlin/cndjp/qicoo/infrastructure/cache/context/RedisContext.kt
@@ -3,9 +3,9 @@ package infrastructure.cache.context
 import redis.clients.jedis.Jedis
 
 object RedisContext {
-    fun setEx(context: Jedis, key: String, ttl: Int, value: String) = context.setex(key, ttl, value)
-    fun get(context: Jedis, key: String): String? = context.get(key)
-    fun del(context: Jedis, key: String): Long = context.del(key)
-    fun find(context: Jedis, key: String): Set<String> = context.keys(key)
-    fun flushAll(context: Jedis) = context.flushAll()
+    fun setEx(context: Jedis, key: String, ttl: Int, value: String) = context.use{it.setex(key, ttl, value)}
+    fun get(context: Jedis, key: String): String? = context.use{it.get(key)}
+    fun del(context: Jedis, key: String): Long = context.use{it.del(key)}
+    fun find(context: Jedis, key: String): Set<String> = context.use{it.keys(key)}
+    fun flushAll(context: Jedis) = context.use{it.flushAll()}
 }
--- src/test/kotlin/cndjp/qicoo/infrastructure/RedisContextSpec.kt
+++ src/test/kotlin/cndjp/qicoo/infrastructure/RedisContextSpec.kt
@@ -8,17 +8,16 @@ import kotlin.test.assertNull
 
 object RedisContextSpec: Spek({
     group("redisのコマンドテスト"){
-        test("始まりのflushAll"){
-            RedisContext.flushAll(qicooGlobalJedisPool.resource)
-        }
         test("正常な値であればSET、GET、DELETEが通る"){
             RedisContext.setEx(qicooGlobalJedisPool.resource, "hoge1", 1000, "piyo")
             assertEquals("piyo", RedisContext.get(qicooGlobalJedisPool.resource, "hoge1"))
             assertEquals(1, RedisContext.del(qicooGlobalJedisPool.resource, "hoge1"))
+            RedisContext.flushAll(qicooGlobalJedisPool.resource)
         }
         test("ない値であればGETはnull"){
             RedisContext.setEx(qicooGlobalJedisPool.resource, "hoge2", 1000, "piyo")
             assertNull(RedisContext.get(qicooGlobalJedisPool.resource, "puga2"))
+            RedisContext.flushAll(qicooGlobalJedisPool.resource)
         }
     }
 })
