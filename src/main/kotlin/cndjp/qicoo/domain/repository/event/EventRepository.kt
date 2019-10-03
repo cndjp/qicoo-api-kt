8c7b887f2ce002be10d65d2273cd41fad7272ee7
nnao45 <n4sekai5y@gmail.com>
2019-10-04 02:03:04 +0900

    　いらんもん消した

--- src/main/kotlin/cndjp/qicoo/domain/repository/event/EventRepository.kt
+++
@@ -1,9 +0,0 @@
-package domain.repository.event
-
-import domain.dao.event.Event
-import java.util.*
-
-interface EventRepository {
-    fun findAll(): List<Event>
-    fun findById(id: UUID): Event?
-}
