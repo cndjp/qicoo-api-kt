9ee16e6d2fa24a97f0e3382486a3f6137e34c6d2
nnao45 <n4sekai5y@gmail.com>
2019-10-06 16:09:00 +0900

    　likeCountRepositoryImpl.findByIdのテスト実装

--- src/test/kotlin/cndjp/qicoo/domain/repository/LikeCountRepositorySpec.kt
+++ src/test/kotlin/cndjp/qicoo/domain/repository/LikeCountRepositorySpec.kt
@@ -1,13 +1,11 @@
 package domain.repository
 
-import domain.dao.like_count.LikeCount
-import domain.dao.like_count.LikeCountList
-import domain.model.like_count.LikeCountRow
 import domain.repository.like_count.LikeCountRepositoryImpl
 import infrastructure.cache.client.qicooGlobalJedisPool
 import infrastructure.cache.context.RedisContext
 import org.spekframework.spek2.Spek
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 object LikeCountRepositorySpec: Spek({
     group("LikeCountRepositoryのテスト") {
@@ -20,6 +18,19 @@ object LikeCountRepositorySpec: Spek({
             assertEquals(1, list[1].question_id)
             assertEquals(2, list[1].count)
             RedisContext.flushAll(qicooGlobalJedisPool.resource)
+        }
+        test("findById()のテスト"){
+            val likeCountRepositoryImpl = LikeCountRepositoryImpl()
+            assertEquals(2, RedisContext.zadd(qicooGlobalJedisPool.resource, likeCountRepositoryImpl.likeCountListKey, mapOf(Pair("1", 2.0), Pair("3", 5.0))))
+            val like1 = likeCountRepositoryImpl.findById(1)
+            val like2 = likeCountRepositoryImpl.findById(2)
+            val like3 = likeCountRepositoryImpl.findById(3)
+            assertNull(like2?.question_id)
+            assertNull(like2?.count)
+            assertEquals(1, like1?.question_id)
+            assertEquals(2, like1?.count)
+            assertEquals(3, like3?.question_id)
+            assertEquals(5, like3?.count)
         }
     }
 })
