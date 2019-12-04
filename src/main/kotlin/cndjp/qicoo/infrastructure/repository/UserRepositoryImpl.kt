1d0149d2bd3282a46ecc1e88326c0770d57366a2
nnao45 <n4sekai5y@gmail.com>
2019-12-04 09:36:46 +0900

    UserRepositoryの下回り実装

--- src/main/kotlin/cndjp/qicoo/infrastructure/repository/UserRepositoryImpl.kt
+++ src/main/kotlin/cndjp/qicoo/infrastructure/repository/UserRepositoryImpl.kt
@@ -6,16 +6,16 @@ import cndjp.qicoo.domain.dao.user.NewUser
 import cndjp.qicoo.domain.repository.user.UserRepository
 import com.github.michaelbull.result.Result
 
-class UserRepositoryImpl: UserRepository {
+class UserRepositoryImpl : UserRepository {
     override fun findLinedUserByGitHubAccountId(gitHubAccountId: Int): Result<LinkedUser, QicooError> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun findLinedUserById(id: Int): Result<LinkedUser, QicooError> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun insert(newUser: NewUser): Result<Unit, QicooError> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
